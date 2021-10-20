45bb8cc1a1878fe02a2e2f3d3993b3ad2cce5c05
tumblerswitch <t.sharipov@myhart.ru>
2021-10-20 22:50:12 +0300

    2

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Danya</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<main id="main">
+    <h1 id="title">Пользовательские истории Дани:</h1>
+    <p>Человек который шутит?</p>
+
+    <div id="img-div">
+        <img class="responsive" src="https://www.kino-teatr.ru/acter/photo/2/9/523692/1145001.jpg" alt="" id="image">
+        <figcaption id="img-caption"><em>Sir Danya.P official photo</em></figcaption>
+    </div>
+
+
+    <section id="tribute-info">
+
+        <h3 id="headline">Жизнь знаменитого Даньки:</h3>
+
+        <ul>
+            <li><strong>1994</strong> - Родился в Воронеже</li>
+            <li>
+                <strong>с 2012 по текущее время </strong> - Шутит.
+            </li>
+            <li>
+                <strong>2021</strong> - Еще жив.
+            </li>
+        </ul>
+
+        <p> Истори Дани гласит что он <s>смешно</s> шутит, убедитесь в этом сами,
+            <a id="tribute-link" href="https://www.youtube.com/c/Spoontamer" target="_blank">устройте рандеву с его
+                творчеством.</a>
+        </p>
+    </section>
+</main>
+</html>
